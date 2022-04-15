eed97eabb1dedf691927c9f684add46979d6c45d
Dipankar Gupta <dipucpi50@gmail.com>
2022-04-15 19:30:28 +0600

    simple calculator using vanilla js for practice

---
+++ script.js
@@ -0,0 +1,66 @@
+let screen=0, operation="", firstOperand=0, secondOperand=0;
+
+function buttonAction(pressedButton)
+{
+    console.log(pressedButton);
+    if (pressedButton == "Ac" || pressedButton == "C") {
+        screen = 0;
+    }
+    else if (pressedButton == "X 2") {
+        screen=Math.pow(screen,2);
+    }
+    else if (pressedButton == "<") {
+        screen = (screen-(screen%10)) / 10;
+    }
+    else if (pressedButton == "/") {
+        operation = "/";
+        firstOperand = screen;
+        screen = 0;
+    }
+    else if (pressedButton == "X") {
+        operation = "*";
+        firstOperand = screen;
+        screen = 0;
+    }
+    else if (pressedButton == "-") {
+        operation = "-";
+        firstOperand = screen;
+        screen = 0;
+    }
+    else if (pressedButton == "=") {
+        secondOperand =screen;
+        if(operation == '+'){
+            screen = firstOperand + secondOperand;
+        }
+        else if(operation == '-'){
+            screen = firstOperand - secondOperand;
+        }
+        else if(operation == '*'){
+            screen = firstOperand * secondOperand;
+        }
+        else if(operation == '/'){
+            screen = firstOperand / secondOperand;
+        }
+        else if(operation == '%'){
+            screen = firstOperand % secondOperand;
+        }
+    }
+    else if (pressedButton == "%") {
+        operation = "%";
+        firstOperand = screen;
+        screen = 0;
+    }
+    else if (pressedButton == "+") {
+        operation = "+";
+        firstOperand = screen;
+        screen = 0;
+    }
+    else {
+        screen = (screen * 10) + parseInt(pressedButton);
+    }
+    updateScreen();
+}
+function updateScreen() {
+    document.getElementById("screen_1").innerText = screen;
+}
+updateScreen();
